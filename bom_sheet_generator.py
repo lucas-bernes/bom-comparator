de1569f17b2ff17ff0125feac6006d277068d8fd
Lucas Bernes <bernesi.bsni@gmail.com>
2026-05-05 10:23:00 -0300

    refactor: generate JLCPCB and Altium BOMs from a common component base

--- bom_sheet_generator.py
+++ bom_sheet_generator.py
@@ -7,132 +7,54 @@ def generate_designator(prefix, count):
     return [f"{prefix}{i}" for i in range(1, count+1)]
 
 
-# def generate_resistor(count):
-#     values = ['1K', '10K', '100K', '470', '4K7', '5K', '500']
-#     footprint = ['0603', '0805']
-
-#     designators = generate_designator('R', count)
-
-#     topCount = np.random.randint(0, count+1)
-
-#     shuffled_designators = np.random.permutation(designators)
-
-#     top_list = shuffled_designators[:topCount]
-#     bottom_list = shuffled_designators[topCount:]
-
-#     component_list = []
-
-#     for i in top_list:
-#         component_list.append({
-#             'Comment': np.random.choice(values),
-#             'topDesignator': i,
-#             'bottomDesignator': None,
-#             'Footprint': np.random.choice(footprint)
-#         })
-
-#     for i in bottom_list:
-#         component_list.append({
-#             'Comment': np.random.choice(values),
-#             'topDesignator': None,
-#             'bottomDesignator': i,
-#             'Footprint': np.random.choice(footprint)
-#         })
-
-#     return component_list
-
-
-# def generate_capacitor(count):
-#     values = ['1u', '10p', '100p', '220n', '100n', '10n', '2200n']
-#     footprint = ['0603', '0805']
-
-#     designators = generate_designator('C', count)
-
-#     return [
-#         {
-#             'Comment': np.random.choice(values),
-#             'Designators': i,
-#             'Footprint': np.random.choice(footprint)
-#         }
-#         for i in designators
-#     ]
-
-
-# def generate_inductor(count):
-#     values = ['1uH', '10uH', '100uH']
-#     footprint = ['0603', '0805']
-
-#     designators = generate_designator('L', count)
-
-#     return [
-#         {
-#             'Comment': np.random.choice(values),
-#             'Designators': i,
-#             'Footprint': np.random.choice(footprint)
-#         }
-#         for i in designators
-#     ]
-
-
-# def generate_power_source(count):
-#     values = ['12V', '5V', '-12V', '-5V', '6V', '9V']
-#     footprint = ['0603', '0805']
-
-#     designators = generate_designator('PS', count)
-
-#     return [
-#         {
-#             'Comment': np.random.choice(values),
-#             'Designators': i,
-#             'Footprint': np.random.choice(footprint)
-#         }
-#         for i in designators
-#     ]
-
-
-# def join_components(df):
-
-#     df_grouped = df.groupby(['Comment', 'Footprint']).agg({
-#         'topDesignator': lambda x: ', '.join(sorted(x.dropna(), key=lambda d: int(d[1:]))),
-#         'bottomDesignator': lambda x: ', '.join(sorted(x.dropna(), key=lambda d: int(d[1:])))
-#     })
-
-#     return df_grouped.reset_index()
-
-def generate_component(prefix, count, values, footprints=('0603', '0805')):
-    designators = generate_designator(prefix, count)
-
-    top_count = np.random.randint(0, count + 1)
-    shuffled_designators = np.random.permutation(designators)
-
-    top_list = shuffled_designators[:top_count]
-    bottom_list = shuffled_designators[top_count:]
-
-    component_list = []
-
-    for designator in top_list:
-        component_list.append({
+def generate_component_base(prefix, count, values, footprints=('0603', '0805')):
+    return [
+        {
             'Comment': np.random.choice(values),
-            'topDesignator': designator,
-            'bottomDesignator': None,
+            'Designator': designator,
             'Footprint': np.random.choice(footprints)
-        })
+        }
+        for designator in generate_designator(prefix, count)
+    ]
 
-    for designator in bottom_list:
-        component_list.append({
-            'Comment': np.random.choice(values),
-            'topDesignator': None,
-            'bottomDesignator': designator,
-            'Footprint': np.random.choice(footprints)
-        })
 
-    return component_list
+df_base = pd.concat([
+    pd.DataFrame(generate_component_base(
+        'R', 30, ['1K', '10K', '100K', '470', '4K7', '5K', '500'])),
+
+    pd.DataFrame(generate_component_base(
+        'C', 30, ['1u', '10p', '100p', '220n', '100n', '10n', '2200n'])),
+
+    pd.DataFrame(generate_component_base(
+        'L', 5, ['1uH', '10uH', '100uH'])),
+
+    pd.DataFrame(generate_component_base(
+        'PS', 3, ['12V', '5V', '-12V', '-5V', '6V', '9V']))
+], ignore_index=True)
+
+
+def make_altium_bom(df_base):
+    return df_base.copy()
+
+
+def make_jlcpcb_bom(df_base):
+    df_jlc = df_base.copy()
+
+    is_top = np.random.choice([True, False], size=len(df_jlc))
+
+    df_jlc['topDesignator'] = df_jlc['Designator'].where(is_top, None)
+    df_jlc['bottomDesignator'] = df_jlc['Designator'].where(~is_top, None)
+
+    df_jlc = df_jlc.drop(columns='Designator')
+
+    return df_jlc
 
 
 def designator_number(designator):
     return int(re.search(r'\d+', designator).group())
 
 
-def join_components(df):
+def join_components_jlcpcb(df):
     df_grouped = df.groupby(['Comment', 'Footprint']).agg({
         'topDesignator': lambda x: ', '.join(sorted(x.dropna(), key=designator_number)),
         'bottomDesignator': lambda x: ', '.join(sorted(x.dropna(), key=designator_number))
@@ -141,15 +63,16 @@ def join_components(df):
     return df_grouped.reset_index()
 
 
-df_jlcpcb = pd.DataFrame()
-df_jlcpcb = pd.concat([
-    pd.DataFrame(generate_component(
-        'R', 30, ['1K', '10K', '100K', '470', '4K7', '5K', '500'])),
-    pd.DataFrame(generate_component(
-        'C', 30, ['1u', '10p', '100p', '220n', '100n', '10n', '2200n'])),
-    pd.DataFrame(generate_component('L', 5, ['1uH', '10uH', '100uH'])),
-    pd.DataFrame(generate_component(
-        'PS', 3, ['12V', '5V', '-12V', '-5V', '6V', '9V']))
-], ignore_index=True)
+def join_components_altium(df):
+    df_grouped = df.groupby(['Comment', 'Footprint']).agg({
+        'Designator': lambda x: ', '.join(sorted(x, key=designator_number))
+    })
 
-print(join_components(df_jlcpcb))
+    return df_grouped.reset_index()
+
+
+df_altium = make_altium_bom(df_base)
+df_jlcpcb = make_jlcpcb_bom(df_base)
+
+print(join_components_altium(df_altium))
+print(join_components_jlcpcb(df_jlcpcb))
