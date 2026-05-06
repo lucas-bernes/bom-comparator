f95dbd8545bc215805ef23144afc89a8b108c9b4
Lucas Bernes <bernesi.bsni@gmail.com>
2026-05-06 13:52:47 -0300

    feat: add find altium bom header function

--- bom_comparator.py
+++ bom_comparator.py
@@ -6,22 +6,32 @@ DATA_DIR = Path('data')
 OUTPUT_DIR = Path('outputs')
 
 
-def find_header_row_altium(file_path):
+def find_header_row_altium(df):
+
+    for index, row in df.iterrows():
+        if 'Designator' in row.astype(str).values:
+            print(f' BOM Header Index: {index} \n')
+            return index
+
+    return None
     # procurar linha onde aparece Comment, Footprint, Designator
-    pass
 
 
 def read_altium_bom(file_path):
+    df = pd.read_excel(file_path)
+    head_index = find_header_row_altium(df)
+    print(df)
+    return df
     # usar find_header_row_altium
     # retornar df normalizado com Comment, Footprint, Designator
-    pass
 
 
 def read_jlcpcb_bom(file_path):
+    df = pd.read_excel(file_path)
+    return df
     # ler direto
     # juntar topDesignator e bottomDesignator em uma coluna Designator
     # retornar df normalizado
-    pass
 
 
 def split_designators(df):
@@ -41,14 +51,13 @@ def export_results(df_result):
 
 
 def main():
-    print(OUTPUT_DIR)
     altium_path = DATA_DIR / 'altium_bom.xlsx'
     jlcpcb_path = DATA_DIR / 'jlcpcb_bom.xlsx'
 
     df_altium = read_altium_bom(altium_path)
     df_jlcpcb = read_jlcpcb_bom(jlcpcb_path)
 
-    df_result = compare_boms(df_altium, df_jlcpcb)
+    # df_result = compare_boms(df_altium, df_jlcpcb)
 
     # export_results(df_result)
 
